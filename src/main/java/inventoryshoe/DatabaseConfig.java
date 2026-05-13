567c19b997177ae2e06412892b50505c488dee3d
daniel-tekle <tekledaniel26@gmail.com>
2026-05-13 12:04:33 -0700

    Create DatabaseConfig.java

---
+++ src/main/java/inventoryshoe/DatabaseConfig.java
@@ -0,0 +1,7 @@
+package inventoryshoe;
+
+public class DatabaseConfig {
+    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String USER = "postgres";
+    public static final String PASS = "0274";
+}
